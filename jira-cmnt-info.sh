0def259e8b5dc89f5b08e9b8ea7cb54a4fe9dc2e
serine <tskiril@gmail.com>
2018-07-24 11:12:25 +1000

    Removed repitable code block, all required bits get inherited from main jira.sh file

--- jira-cmnt-info.sh
+++ jira-cmnt-info.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
 jira_exec="jira"
-jira_chk="jira-check"
-
-jira_usr=${JIRA_USER}
-jira_pass=${JIRA_PASSWORD}
-jira_proj=${JIRA_PROJECT}
-jira_server=${JIRA_SERVER}
-
-
-if [[ -s ${jira_chk} ]]
-then
-  echo "ERROR: This shouldn't happend, ${jira_chk} not found"
-else
-  ${jira_chk}
-  if [[ ${PIPESTATUS[0]} != 0 ]]
-  then
-    exit
-  fi
-fi
-
 action="getComments"
 
 if [[ -z "$@" ]]
--- jira-state-info.sh
+++ jira-state-info.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
 jira_exec="jira"
-jira_chk="jira-check"
-
-jira_usr=${JIRA_USER}
-jira_pass=${JIRA_PASSWORD}
-jira_proj=${JIRA_PROJECT}
-jira_server=${JIRA_SERVER}
-
-if [[ -s ${jira_chk} ]]
-then
-  echo "ERROR: This shouldn't happend, ${jira_chk} not found"
-else
-  ${jira_chk}
-  if [[ ${PIPESTATUS[0]} != 0 ]]
-  then
-    exit
-  fi
-fi
-
 action="getIssueList"
 
 usage() {
@@ -79,6 +61,9 @@ case "${1}" in
   (follow|f)
     issue_type="AND status='Follow Up'"
     ;;
+  #(overview|o)
+  #  issue_type="AND status!='Done'"
+  #  ;;
   (*)
     usage
 esac
